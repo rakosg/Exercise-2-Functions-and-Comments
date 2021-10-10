c649b50cfe9eb12d9bb62958199ec11da82a288e
rakosg <rakog@oregonstate.edu>
2021-10-10 14:48:58 -0700

    need to edit the functions

--- main.rb
+++ main.rb
@@ -4,24 +4,16 @@
 
 # reverse the name or replace it with "borg"
 def modified_name(choose)
-  # Split the name into first and last in an array
   split_name = @name.split
-  
-  # Reverse the array of name components so the array is [last, first]
   reversed_name_components = split_name.reverse
-  # Rejoin the array of name components
   new_name1 = reversed_name_components.join(' ')
-  # Store the mangled name
   @name = new_name1
   puts new_name1
+  # can either split a name into a first and last name or add "Borg" to the end
   if choose
-    # Split the name into last and first in an array
     split_name2 = new_name1.split
-    # Reverse the array of name components so the array is [last, first]
     reversed_name_components2 = split_name2.reverse
-    # Add borg to the end of the array
     reversed_name_components2 << "Borg"
-    # Rejoin the array of name components
     new_name2 = reversed_name_components2.join(' ')
     puts new_name2
     @name = new_name2
